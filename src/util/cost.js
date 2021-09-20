3410ddf11677221228d8fa074694cead3178aed3
Daniel Michael <itzdanielmichael@gmail.com>
2021-09-20 18:03:32 -0400

    create cost.test.js

--- src/util/cost.js
+++ src/util/cost.js
@@ -4,6 +4,6 @@ for 11 to 100 notes, we'll charge $2 and any more than 100 is $1 per note. Since
 stripe expects us to provide the amount in pennies (the currency's smallest unit) we multiply the result by 100.
 */
 export function calculateCost(storage) {
-  const rate = storage <= 10 ? 4 : storage < 100 ? 2 : 1;
+  const rate = storage <= 10 ? 4 : storage <= 100 ? 2 : 1;
   return rate * storage * 100;
 }
