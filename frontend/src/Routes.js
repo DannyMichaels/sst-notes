8ae9e020c803f6c4981d371479db2b5b61ae8715
Daniel Michael <itzdanielmichael@gmail.com>
2021-09-20 21:00:46 -0400

    create NotFOund.js

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,12 +1,12 @@
 import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
+import NotFound from './containers/NotFound';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
+      <Route exact path="/" component={Home} />
+      <Route component={NotFound} />
     </Switch>
   );
 }
