32af9be27abe275794bc649df7101731c2cc0162
Daniel Michael <itzdanielmichael@gmail.com>
2021-09-20 21:18:56 -0400

    update Routes.js, create Login page

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,11 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
+import Login from './containers/Login';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
       <Route component={NotFound} />
     </Switch>
   );
---
+++ frontend/src/containers/Login.js
@@ -0,0 +1,44 @@
+import { useState, useMemo } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './Login.css';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const isFormValid = useMemo(
+    () => email.length > 0 && password.length > 0,
+    [email, password]
+  );
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!isFormValid}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
