f60249b2af04e3612e016fa7408a260253803095
Daniel Michael <itzdanielmichael@gmail.com>
2021-09-21 10:36:03 -0400

    update Routes.js create Signup.js

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,12 +2,14 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
 import Login from './containers/Login';
+import Signup from './containers/Signup';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
       <Route component={NotFound} />
     </Switch>
   );
